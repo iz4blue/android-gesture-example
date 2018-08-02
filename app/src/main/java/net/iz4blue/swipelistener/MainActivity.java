7ce7cc49a063fd9ce0c4c2448cafdb71b0e45197
김정환 <iz4blue@users.noreply.github.com>
2018-08-02 20:45:25 +0900

    제스쳐 관련 소스 예시로 추가

---
+++ app/src/main/java/net/iz4blue/swipelistener/MainActivity.java
@@ -0,0 +1,37 @@
+package net.iz4blue.swipelistener;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.hello_word);
+
+        textView.setOnTouchListener(new OnSwipeTouchListener(getBaseContext()) {
+            public void onSwipeTop() {
+                Toast.makeText(MainActivity.this, "top", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeLeft() {
+                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeBottom() {
+                Toast.makeText(MainActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+
+}
